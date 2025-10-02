78e530b6213497f73b1fa864aebd8eed15946413
Sufiya Sayyed <ssayyed01@gmail.com>
2025-10-02 17:51:54 +0400

    initial commit setup

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/research")
+def researchCheck():
+    return {"message":"ok"}
+
+def main():
+    print("Hello from ai-research-paper-summarizer!")
+
+
+if __name__ == "__main__":
+    main()
