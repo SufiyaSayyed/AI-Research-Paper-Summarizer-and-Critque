d62d0331ebcf6acf30ab478c137566f4d7a7ab1d
Sufiya Sayyed <ssayyed01@gmail.com>
2025-10-04 19:14:00 +0400

    exposing summary route in main

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .auth.route import router as auth_router
 from .papers.route import router as paper_router
+from .summary.route import router as summary_router
 
 app = FastAPI(title="Research Summarizer")
 
@@ -14,3 +15,4 @@ app.add_middleware(
 
 app.include_router(auth_router)
 app.include_router(paper_router)
+app.include_router(summary_router)
